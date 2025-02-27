9c1aa683e1ea4347f2ca1e5da59e7fb93cea97e8
Vitalina <vsinkovaa@gmail.com>
2025-02-27 21:56:16 +0200

    hw06

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,3 @@
-// Задача 1. Акаунт користувача
-// Перед звільненням розробник зламав вихідний код
-// управління акаунтами користувачів нашого сервісу доставки їжі.
-// Виконай рефакторинг методів об'єкта customer,
-// розставивши відсутні this під час звернення до властивостей об'єкта.
-
-// Використай цей стартовий код і виконай рефакторинг.
-// Після оголошення об'єкта ми додали виклики методів.
-// У консоль будуть виведені результати їх роботи.
-// Будь ласка, нічого там не змінюй.
-
 const customer = {
   username: 'Mango',
   balance: 24000,
@@ -39,18 +28,3 @@ console.log(customer.getDiscount()); // 0.15
 customer.addOrder(5000, 'Steak');
 console.log(customer.getBalance()); // 19750
 console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
-
-// На що буде звертати увагу ментор при перевірці:
-
-// Оголошена змінна customer
-// Значення змінної customer — це об'єкт із властивостями та методами
-// Виклик customer.getDiscount() повертає поточне значення властивості discount
-// Виклик customer.setDiscount(0.15) оновлює значення властивості discount
-// Виклик customer.getBalance() повертає поточне значення властивості balance.
-// Виклик customer.getOrders() повертає поточне значення властивості orders
-// Виклик customer.addOrder(5000, "Steak") додає "Steak" у масив значень властивості orders та оновлює баланс
-// Метод getBalance об'єкта customer використовує this
-// Метод getDiscount об'єкта customer використовує this
-// Метод setDiscount об'єкта customer використовує this
-// Метод getOrders об'єкта customer використовує this
-// Метод addOrder об'єкта customer використовує this
